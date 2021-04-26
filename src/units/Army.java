5efcf19b1664d26bc9bfd8dd163c84104a2651d3
OmarMelouk <okmelouk@gmail.com>
2021-04-26 23:02:25 +0200

    saved classes, no other change

--- src/units/Army.java
+++ src/units/Army.java
@@ -68,4 +68,6 @@ public int getMaxToHold() {
 
 
 
+
+
 }
--- src/units/Infantry.java
+++ src/units/Infantry.java
@@ -1,5 +1,13 @@
 package units;
 
-public class Infantry {
+public class Infantry extends Unit {
+
+	public Infantry(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep) {
+		super(level, maxSoldierCount, idleUpkeep, marchingUpkeep, siegeUpkeep);
+		// TODO Auto-generated constructor stub
+	}
+	
+	//csv file
+	
 
 }
